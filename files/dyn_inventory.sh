dbc3bcccab5acb4db1b039b8a182d39b76b4da22
forenkla <jens@forenkla.com>
2021-12-30 13:41:08 +0100

    testing

--- files/dyn_inventory.sh
+++ files/dyn_inventory.sh
@@ -4,19 +4,22 @@
 
 
 
-local HOST_LIST=$(prlctl list | /usr/bin/awk -v col=4 '{print $col}' | /usr/bin/awk 'NR>1');
-local HOSTS="";
-local HOST_VARS="";
+#HOST_LIST=$(prlctl list | /usr/bin/awk -v col=4 '{print $col}' | /usr/bin/awk 'NR>1');
+HOST_LIST="ansible12"
+IP_LIST="255.255.255.255"
+HOSTS="";
+HOST_VARS="";
 
 #echo $HOST_LIST;
 
-local IP_LIST=$(echo "$HOST_LIST" | xargs -I{} prlctl exec {} ip -4 -br addr show enp0s5 | awk '{print $3}' | cut -d / -f 1);
+
+#IP_LIST=$(echo "$HOST_LIST" | xargs -I{} prlctl exec {} ip -4 -br addr show enp0s5 | awk '{print $3}' | cut -d / -f 1);
 
 
-local ALL_INFO=$(paste <(echo "$HOST_LIST") <(echo "$IP_LIST"));
+ALL_INFO=$(paste <(echo "$HOST_LIST") <(echo "$IP_LIST"));
 
-local NUM_OF_HOSTS=$(echo "$HOST_LIST" | wc -l);
-local n=1;
+NUM_OF_HOSTS=$(echo "$HOST_LIST" | wc -l);
+n=1;
 while read -r vm; do
         if [[ "$NUM_OF_HOSTS" -gt "$n" ]]; then
                         HOSTS="$HOSTS \"$(echo "$vm" |  awk 'BEGIN {FS="\t"}; {print $1}')\", ";
