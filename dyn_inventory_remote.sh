059a33677fd5eb3b18ddcb954740237f9186190f
forenkla <jens@forenkla.com>
2021-12-30 06:40:38 +0100

    Testing

--- dyn_inventory_remote.sh
+++ dyn_inventory_remote.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 # id: scripts/trivial-inventory-script.sh
 
-HOST_LIST=$(/usr/local/bin/prlctl list | /usr/bin/awk -v col=4 '{print $col}' | /usr/bin/awk 'NR>1')
+HOST_LIST=$(ssh jboivie@192.168.1.31 '/usr/local/bin/prlctl list | /usr/bin/awk -v col=4 '"'"'{print $col}'"'"' | /usr/bin/awk '"'"'NR>1'"'"'')
 
 #echo $HOST_LIST;
 
-IP_LIST=$('/usr/bin/xargs -I{} /usr/local/bin/prlctl exec {} ip -4 -br addr show enp0s5 | awk '{print $3}' | cut -d / -f 1)
+IP_LIST=$(echo "$HOST_LIST" | ssh jboivie@192.168.1.31 '/usr/bin/xargs -I{} /usr/local/bin/prlctl exec {} ip -4 -br addr show enp0s5 | awk '"'"'{print $3}'"'"' | cut -d / -f 1')
 
+
+# #paste <(echo "$HOST_LIST") <(echo "$IP_LIST")
 
 ALL_INFO=$(paste <(echo "$HOST_LIST") <(echo "$IP_LIST"))
 
@@ -14,6 +16,7 @@ NUM_OF_HOSTS=$(echo "$HOST_LIST" | wc -l)
 n=1
 while read -r vm; do
         if [[ "$NUM_OF_HOSTS" -gt "$n" ]]; then
+                        #HOSTS="$HOSTS \"$vm\", "
                         HOSTS="$HOSTS \"$(echo "$vm" |  awk 'BEGIN {FS="\t"}; {print $1}')\", "
                         HOST_VARS="$HOST_VARS \"$(echo "$vm" |  awk 'BEGIN {FS="\t"}; {print $1}')\": { \"ansible_host\": \"$(echo "$vm" | awk 'BEGIN {FS="\t"}; {print $2}')\" },"
         else
@@ -24,10 +27,10 @@ while read -r vm; do
 done <<< "$ALL_INFO"
 
 echo "{"
-echo   "\"all\": {"  
-echo        "\"hosts\": [" 
-echo $HOSTS ;
-echo   "] }," 
-echo   " \"_meta\": {" 
-echo       " \"hostvars\": {" 
-echo $HOST_VARS "}}}" 
+echo   "\"all\": {"
+echo        "\"hosts\": ["
+echo $HOSTS;
+echo   "] },"
+echo   " \"_meta\": {"
+echo       " \"hostvars\": {"
+echo $HOST_VARS "}}}"
