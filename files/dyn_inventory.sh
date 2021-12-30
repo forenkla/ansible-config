7cd02df863add10636b3a940c8308b1481ca0c7b
forenkla <jens@forenkla.com>
2021-12-30 10:34:53 +0100

    Semi

--- files/dyn_inventory.sh
+++ files/dyn_inventory.sh
@@ -5,32 +5,32 @@
 
 
 HOST_LIST=$(prlctl list | /usr/bin/awk -v col=4 '{print $col}' | /usr/bin/awk 'NR>1');
-HOSTS=""
-HOST_VARS=""
+HOSTS="";
+HOST_VARS="";
 
 #echo $HOST_LIST;
 
 IP_LIST=$(echo "$HOST_LIST" | xargs -I{} prlctl exec {} ip -4 -br addr show enp0s5 | awk '{print $3}' | cut -d / -f 1);
 
 
-ALL_INFO=$(paste <(echo "$HOST_LIST") <(echo "$IP_LIST"))
+ALL_INFO=$(paste <(echo "$HOST_LIST") <(echo "$IP_LIST"));
 
-NUM_OF_HOSTS=$(echo "$HOST_LIST" | wc -l)
-n=1
+NUM_OF_HOSTS=$(echo "$HOST_LIST" | wc -l);
+n=1;
 while read -r vm; do
         if [[ "$NUM_OF_HOSTS" -gt "$n" ]]; then
-                        HOSTS="$HOSTS \"$(echo "$vm" |  awk 'BEGIN {FS="\t"}; {print $1}')\", "
-                        HOST_VARS="$HOST_VARS \"$(echo "$vm" |  awk 'BEGIN {FS="\t"}; {print $1}')\": { \"ansible_host\": \"$(echo "$vm" | awk 'BEGIN {FS="\t"}; {print $2}')\" },"
+                        HOSTS="$HOSTS \"$(echo "$vm" |  awk 'BEGIN {FS="\t"}; {print $1}')\", ";
+                        HOST_VARS="$HOST_VARS \"$(echo "$vm" |  awk 'BEGIN {FS="\t"}; {print $1}')\": { \"ansible_host\": \"$(echo "$vm" | awk 'BEGIN {FS="\t"}; {print $2}')\" },";
         else
-                        HOSTS="$HOSTS \"$(echo "$vm" |  awk 'BEGIN {FS="\t"}; {print $1}')\""
-                        HOST_VARS="$HOST_VARS \"$(echo "$vm" |  awk 'BEGIN {FS="\t"}; {print $1}')\": { \"ansible_host\": \"$(echo "$vm" | awk 'BEGIN {FS="\t"}; {print $2}')\" }"
+                        HOSTS="$HOSTS \"$(echo "$vm" |  awk 'BEGIN {FS="\t"}; {print $1}')\"";
+                        HOST_VARS="$HOST_VARS \"$(echo "$vm" |  awk 'BEGIN {FS="\t"}; {print $1}')\": { \"ansible_host\": \"$(echo "$vm" | awk 'BEGIN {FS="\t"}; {print $2}')\" }";
         fi
         let n=n+1;
 done <<< "$ALL_INFO"
 
 echo "{"
-echo   "\"all\": {"  
-echo        "\"hosts\": [" 
+echo   "\"all\": {" 
+echo        "\"hosts\": ["
 echo $HOSTS ;
 echo   "] }," 
 echo   " \"_meta\": {" 
